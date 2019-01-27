d7f82b6f0a69077bbd603bb56695e5bf8ffdb64d
Ben Parisot <me@benparisot.com>
2019-01-27 09:52:34 -0800

    get typewriter functions to work for welcome message

--- main.js
+++ main.js
@@ -2,6 +2,43 @@ function add(a, b) {
     return a + b;
 }
 
+var i = 0;
+var welcome = 'Welcome. I am an Automated Isogram Test. ';
+var definition = 'An isogram (also known as a "nonpattern word") is a logological term for a word or phrase without a repeating letter. ';
+var offer = 'Enter your favorite word and I will tell you if it is an isogram or not. ';
+var results = '';
+// var blinkingCursor = ' | ';
+var speed = 50;
+
+
+function start() {
+  if (i < welcome.length) {
+    document.getElementById("welcome").innerHTML += welcome.charAt(i);
+    i++;
+    setTimeout(start, speed);
+  }
+
+  if (i < definition.length) {
+    document.getElementById("definition").innerHTML += definition.charAt(i);
+    i++;
+    setTimeout(start, speed);
+  }
+
+  if (i < offer.length) {
+    document.getElementById("offer").innerHTML += offer.charAt(i);
+    i++;
+    setTimeout(start, speed);
+  }
+  
+//   if (i === welcome.length) {
+//   document.getElementById("blinking-cursor").innerHTML = blinkingCursor;
+//   }
+}
+
+
+
+
+
 function isIsogram(){    
     var word = document.getElementById('word').value; 
     var wordArray = word.split('');
